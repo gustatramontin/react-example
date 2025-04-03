99de0d26363eedd4f7c5abf11175fdc8f7c08baf
Gustavo Tramontin Pedro <mrgustavotramontin@gmail.com>
2025-04-03 13:06:54 -0300

    refac(use async await on listagem.js)

--- src/app/components/listagem.js
+++ src/app/components/listagem.js
@@ -5,22 +5,25 @@ export default function ListagemDePessoas() {
     let [pessoas, setPessoas] = useState([])
     let [erro, setErro] = useState("")
 
-    function getPessoas() {
-	fetch('http://localhost:5000/listar_pessoas')
-                .then(response => response.json()) // especificando que a resposta vai ser em json
-                .then(json => {
-                    //console.log(json);
-                    if (json.resultado == 'ok') { // a API informa que deu certo a chamada?
-                        setPessoas(json.detalhes); // pega as pessoas ;-p
-                    } else {
-                        // informar o erro - assustar o usuário :-O
-                        setPessoas([{"nome":json.detalhes, "telefone":"erro", "email":"erro"}])
-                    }
-                })
-                .catch(error => {
-                    setErro(error)
-                });
-        }
+    async function getPessoas() {
+	try {
+	    const res = await fetch('http://localhost:5000/listar_pessoas')
+	    const json = await res.json()
+
+	    if (json.resultado == 'ok') { // a API informa que deu certo a chamada?
+		setPessoas(json.detalhes); // pega as pessoas ;-p
+	    } else {
+		// informar o erro - assustar o usuário :-O
+		setPessoas([{"nome":json.detalhes, "telefone":"erro", "email":"erro"}])
+	    }
+
+	} catch (error) {
+	    setErro(error.toString())
+	    return
+	}
+
+
+    }
     return (
 	<>
 	<button className={[styles.botao, "m-3"]} onClick={getPessoas}> Listar Pessoas </button>
