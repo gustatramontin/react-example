cd28386802fdb3cda7516e069d6fe8c56b967fa2
Gustavo Tramontin Pedro <mrgustavotramontin@gmail.com>
2025-04-03 13:00:27 -0300

    added post request to add person

---
+++ src/app/components/form.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import styles from './form.module.css'
+
+export default function Form() {
+
+    let [mensagem, setMensagem] = useState("")
+
+    let [nome, setNome] = useState("João Silva Oliveira")
+    let [email, setEmail] = useState("joaosilva@gmail.com")
+    let [telefone, setTelefone] = useState("47 91234 5678")
+
+    const handleChange = (e, set) => {
+	set(e.target.value)	
+    }
+
+    async function incluirPessoa(formData) {
+
+	const dados = {nome, email, telefone}
+	console.log(dados)
+	try {
+	const res = await fetch("http://localhost:5000/incluir_pessoa", {
+	    method: "POST",
+	    headers: {
+		"content-type": "application/json"
+	    },
+	    body: JSON.stringify(dados),
+	})
+	const json = await res.json()
+	console.log(json)
+	if (json.resultado == "ok") {
+	    setMensagem("Pessoa incluída com sucesso!")
+
+	    setNome("")
+	    setEmail("")
+	    setTelefone("")
+	}
+	} catch (error) {
+	    setMensagem(error.toString())
+	}
+
+
+    } 
+    return (
+	<form action={incluirPessoa} className={styles.form}>
+	<h2>Formulario</h2>
+	<p>Nome <input type="text"  value={nome}     onChange={(e) => hangleChange(e, setNome)    }/>
+	</p>
+	<p> Email: <input type="email" value={email}    onChange={(e) => hangleChange(e, setEmail)   }/> </p>
+	<p>Telefone: <input type="tel"   value={telefone} onChange={(e) => hangleChange(e, setTelefone)}/></p>
+
+	<button type="submit">Incluir!</button>
+	<p>{mensagem}</p>
+	</form>
+    )
+}
+
--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import ListagemDePessoas from "./components/listagem"
+import Form from "./components/form"
 
 export default function Home() {
 
   return (
       <div className={styles.main}>
-    
-      <h1>Listagem de pessoas</h1>
-
+       <h1>Listagem de pessoas</h1>
       <ListagemDePessoas />
+      <Form />
 
       </div>
   );
